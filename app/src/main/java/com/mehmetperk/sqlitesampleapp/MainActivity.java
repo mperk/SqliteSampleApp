cbf0600453b3ca0e3e19dc81d7c6b08353c8542b
mperk <P_E_R_K@hotmail.com>
2015-11-28 10:19:57 +0200

    Completed

---
+++ app/src/main/java/com/mehmetperk/sqlitesampleapp/MainActivity.java
@@ -0,0 +1,150 @@
+package com.mehmetperk.sqlitesampleapp;
+
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper Db;
+    EditText editName, editSurname, editMarks, editId;
+    Button btnAddData;
+    Button btnViewAll;
+    Button btnViewUpdate;
+    Button btnDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Db = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editSurname = (EditText) findViewById(R.id.editText_surname);
+        editMarks = (EditText) findViewById(R.id.editText_marks);
+        editId = (EditText) findViewById(R.id.editText_id);
+        btnAddData = (Button) findViewById(R.id.button_add);
+        btnViewAll = (Button) findViewById(R.id.button_viewAll);
+        btnViewUpdate = (Button) findViewById(R.id.button_update);
+        btnDelete = (Button) findViewById(R.id.button_delete);
+        AddData();
+        viewAll();
+        UpdateData();
+        DeleteData();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void DeleteData(){
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer deletedRows = Db.deleteData(editId.getText().toString());
+                if(deletedRows>0)
+                    Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void UpdateData() {
+        btnViewUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdate = Db.updateData(editId.getText().toString(), editName.getText().toString(),
+                        editSurname.getText().toString(), editMarks.getText().toString());
+                if (isUpdate)
+                    Toast.makeText(MainActivity.this, "Data Update", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Update", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void AddData() {
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = Db.insertData(editName.getText().toString(),
+                        editSurname.getText().toString(),
+                        editMarks.getText().toString());
+                if (isInserted)
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void viewAll() {
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = Db.getAllData();
+                if (res.getCount() == 0) {
+                    //show message
+                    showMessage("Error", "Nothing Found");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()) {
+                    buffer.append("Id :" + res.getString(0) + "\n");
+                    buffer.append("Name :" + res.getString(1) + "\n");
+                    buffer.append("Surname :" + res.getString(2) + "\n");
+                    buffer.append("Marks :" + res.getString(3) + "\n");
+                }
+                //show all Data
+                showMessage("Data", buffer.toString());
+            }
+        });
+    }
+
+    public void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
